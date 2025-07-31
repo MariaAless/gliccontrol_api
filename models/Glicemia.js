216306c9a784684eaa5d743b61fd1aa9fc0f4108
Alessandra <alessandraaquino2003@gmail.com>
2025-07-31 17:19:43 -0300

    tabela Glicemia

---
+++ models/Glicemia.js
@@ -0,0 +1,39 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+const Usuario = require('./Usuario');
+
+const Glicemia = sequelize.define('Glicemia', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  valor: {
+    type: DataTypes.FLOAT,
+    allowNull: false
+  },
+  dataHora: {
+    type: DataTypes.DATE,
+    allowNull: false
+  },
+  periodo: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  observacao: {
+    type: DataTypes.TEXT,
+    allowNull: true
+  },
+  usuarioId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'Usuario',
+      key: 'id'
+    }
+  }
+});
+
+Glicemia.belongsTo(Usuario, { foreignKey: 'usuarioId' });
+
+module.exports = Glicemia;
